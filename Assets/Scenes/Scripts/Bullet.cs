58cd6dcbb07e64ca7cc3b93bd2f215fed0f6cdfa
AlbertComb <70507584+AlbertComb@users.noreply.github.com>
2020-10-10 10:55:16 +0200

    Bullet script, Bullet Effect, Shooting
    Da rivedere parzialmente locazione firepoint

---
+++ Assets/Scenes/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Transform target;
+    public float speed = 70f;
+    public GameObject impactEffect;
+
+    public void Search(Transform _target) {
+
+        target = _target;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) {
+            Destroy(gameObject);
+            return;
+                }
+        Vector3 dir = target.position - transform.position;
+        float distanceFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distanceFrame) {
+
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceFrame, Space.World);
+    }
+
+    void HitTarget() {
+        GameObject effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 2f);
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+    }
+}
