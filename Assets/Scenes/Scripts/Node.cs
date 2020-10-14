bb8ca4fac015b63e10496f7771a8d98396a2b3e6
Bad-Karma00 <72249553+Bad-Karma00@users.noreply.github.com>
2020-10-14 09:39:03 +0200

    Shop ed icone

--- Assets/Scenes/Scripts/Node.cs
+++ Assets/Scenes/Scripts/Node.cs
@@ -1,34 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
     public Color hoverColor;
 
     public Vector3 positionOffset;
+   
+    private CanvasGroup shop;
 
     private GameObject turret;
 
     private Renderer rend;
 
     private Color startColor;
+
+    BuildManager buildManager;
     private void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+        buildManager = BuildManager.instance;
     }
 
     private void OnMouseDown()
     {
-        rend.material.color = hoverColor;
+
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+        
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
+
         if (turret != null)
         {
             Debug.Log("Un oca è già qui!");
 
         }
-        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
-        turret = (GameObject) Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+        if (buildManager.GetTurretToBuild() != null && turret == null) {
+            GameObject turretToBuild = buildManager.GetTurretToBuild();
+            turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+            rend.material.color = hoverColor;
+        }
     }
     private void OnMouseExit()
     {
---
+++ Assets/Scenes/Scripts/Shop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    BuildManager buildManager;
+    
+
+    private void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+    public void PurchaseStandardTurret()
+    {
+        Debug.Log("Oca con pistola selezionata");
+        buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
+    }
+
+    public void PurchaseRocketTurret()
+    {
+        Debug.Log("Oca con lanciarazzi selezionata");
+        buildManager.SetTurretToBuild(buildManager.rocketTurretPrefab);
+    }
+
+    public void PurchaseLaserTurret()
+    {
+        Debug.Log("Oca laser selezionata");
+    }
+
+}
