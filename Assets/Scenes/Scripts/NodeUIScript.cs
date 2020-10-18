fb1e91d4089c6ec0e62d2ad9e63817e2f620c01d
Bad-Karma00 <72249553+Bad-Karma00@users.noreply.github.com>
2020-10-18 18:47:30 +0200

    Bottone per migliorare o vendere torrette
    Per ora è solo il pulsante che compare se viene toccato un nodo con sopra una torretta, da implementare le funzionalità dei pulsanti

---
+++ Assets/Scenes/Scripts/NodeUIScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeUIScript : MonoBehaviour
+{
+
+    public GameObject UI;
+
+    private Node target;
+
+    public void SetTarget(Node _target)
+    {
+        UI.SetActive(true);
+        target = _target;
+        transform.position = target.GetBuildPosition();
+    }
+
+    public void Hide()
+    {
+        UI.SetActive(false);
+    }
+}
