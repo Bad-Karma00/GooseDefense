903aada12cc350a91a78b5149d84bbac4fe62cdf
Bad-Karma00 <72249553+Bad-Karma00@users.noreply.github.com>
2020-10-20 12:47:47 +0200

    Menù di pausa

---
+++ Assets/Scenes/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject UI;
+    public void Toggle()
+    {
+        UI.SetActive(!UI.activeSelf);
+
+        if (UI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Da implementare");
+    }
+}
