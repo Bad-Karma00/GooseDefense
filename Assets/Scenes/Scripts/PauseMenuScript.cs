677f6e6d974c04846e4bb5862928d454eadf5bf6
Bad-Karma00 <72249553+Bad-Karma00@users.noreply.github.com>
2020-11-02 12:08:11 +0100

    Import di assets per livelli

---
+++ Assets/Scenes/Scripts/PauseMenuScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public GameObject UI;
+
+    public SceneFader sceneFader;
+
+    public string menuSceneName = "Main Menu";
+
+    public void Toggle()
+    {
+        UI.SetActive(!UI.activeSelf);
+
+        if (UI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        sceneFader.FadeTo(SceneManager.GetActiveScene().name);
+    }
+
+    public void Menu()
+    {
+        Toggle();
+        sceneFader.FadeTo(menuSceneName);
+    }
+}
